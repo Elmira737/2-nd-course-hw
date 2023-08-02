d7aee10efba36e6d959bdf721706f4b483c240aa
Elmira737 <139793523+Elmira737@users.noreply.github.com>
2023-08-02 19:28:53 +0500

    Задание 7

--- hw-4.js
+++ hw-4.js
@@ -81,3 +81,30 @@ function inputValue() {
 }
 
 console.log(inputValue());
+
+function getCircleArea() {
+    return Math.PI * this.radius ** 2;
+}
+
+function getCirclePerimeter() {
+    return 2 * Math.PI * this.radius;
+}
+
+const circle1 = {
+    radius: 7,
+
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+};
+
+const circle2 = {
+    radius: 3,
+
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+};
+
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
