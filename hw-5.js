bb2cdc03808c026bf18c50c292b8869516ee5175
Elmira737 <139793523+Elmira737@users.noreply.github.com>
2023-08-04 21:38:11 +0500

    Задача 2

--- hw-5.js
+++ hw-5.js
@@ -1,6 +1,10 @@
 const numbs = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < numbs.length; i++) {
-console.log(numbs[i]);
-if (numbs[i] == 10) break;
+    console.log(numbs[i]);
+    if (numbs[i] == 10) break;
 }
+
+const arr = [1, 5, 4, 10, 0, 3];
+const index = arr.indexOf(4);
+console.log(index);
