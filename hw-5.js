5f92d66a8a5ae4ca7fa078a4ea3b13880bb8f69f
Elmira737 <139793523+Elmira737@users.noreply.github.com>
2023-08-04 21:45:05 +0500

    Задание 3

--- hw-5.js
+++ hw-5.js
@@ -8,3 +8,7 @@ for (let i = 0; i < numbs.length; i++) {
 const arr = [1, 5, 4, 10, 0, 3];
 const index = arr.indexOf(4);
 console.log(index);
+
+let array = [1, 3, 5, 10, 20];
+array = array.join(' ');
+console.log(array);
