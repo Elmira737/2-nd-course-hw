4a5d403ea7df7859316ac7b30743ab22f024e643
Elmira737 <139793523+Elmira737@users.noreply.github.com>
2023-08-08 20:20:40 +0500

    Задание 2

--- hw-6.js
+++ hw-6.js
@@ -2,3 +2,11 @@ let str = 'js';
 str = str.toUpperCase();
 console.log(str.toUpperCase());
 
+const arr = ['Правило', 'План', 'Производство', 'Профиль', 'Перевод']; 
+const search = 'про';
+
+arr.forEach((arr) => {
+if (arr.toLowerCase().startsWith(search.toLowerCase())) {
+console.log(arr);
+}
+});
