452500962d1866cbcd1714ff3a8c865590f0e6fb
Elmira737 <139793523+Elmira737@users.noreply.github.com>
2023-08-12 01:46:59 +0500

    Задание 2

--- hw-7.js
+++ hw-7.js
@@ -3,8 +3,35 @@ const people = [
     { name: 'Анна', age: 17 },
     { name: 'Олег', age: 7 },
     { name: 'Оксана', age: 47 }
-    ];
-    
-    console.log(people.sort((a, b) => a.age - b.age));
+];
 
-    
+console.log(people.sort((a, b) => a.age - b.age));
+
+function isPositive(num) {
+    return num > 0;
+}
+
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(arr, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i]))
+            result.push(arr[i]);
+    }
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const users = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(users, isMale));
+
